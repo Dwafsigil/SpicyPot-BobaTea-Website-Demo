3f89e831d8f09636e762a5f09dbb3989cc7160ba
Dwafsigil <jtu02@jaguar.tamu.edu>
2026-03-14 23:47:16 -0500

    Redirected the links to correct restaurant.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const menuOverlay = document.querySelector(".menu-overlay");
 const menuQuit = document.querySelector(".menu-quit");
+const header = document.querySelector("header");
 
 menuToggle.addEventListener("click", () => {
   menuOverlay.classList.remove("hidden");
@@ -10,4 +11,12 @@ menuToggle.addEventListener("click", () => {
 menuQuit.addEventListener("click", () => {
   menuOverlay.classList.add("hidden");
   document.body.classList.remove("menu-open");
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 156) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
 });
